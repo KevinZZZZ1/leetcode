9bb1a671722ab4a1d618ca77800acf1f393b5c07
Kevin <zuoyenan@gmail.com>
2019-06-21 21:17:06 +0800

    leetcode Num94

---
+++ src/com/leetcode/tree/Num94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,37 @@
+package com.leetcode.tree;
+// Given a binary tree, return the inorder traversal of its nodes' values
+
+
+import java.util.List;
+import java.util.*;
+
+// 给定一个二叉树，不使用递归的方式求出其中序遍历
+public class Num94_BinaryTreeInorderTraversal {
+    // 思路是：利用栈进行递归模拟
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> ans = new LinkedList<Integer>();
+        if(root==null)
+            return ans;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        TreeNode cur = root;
+        while(!stack.empty() || cur!=null){
+            if(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }else{
+                cur = stack.pop();
+                ans.add(cur.val);
+                cur = cur.right;
+            }
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
