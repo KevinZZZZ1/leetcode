0d51f9d2d4bb2e2cea338cfbc50e1853b804ba65
Kevin <zuoyenan@gmail.com>
2019-07-09 20:49:43 +0800

    leetcode Num326

--- src/com/leetcode/math/Num693_BinaryNumberwithAlternatingBits.java
+++ src/com/leetcode/math/Num693_BinaryNumberwithAlternatingBits.java
@@ -3,7 +3,7 @@ package com.leetcode.math;
 // 判断一个数的位表示是否不会出现连续的 0 和 1
 public class Num693_BinaryNumberwithAlternatingBits {
 
-    // 思路是：对于 1010 这种位级表示的数，把它向右移动 1 位得到 101，
+    // 思路是：对于 1010 这种二进制位表示的数，把它向右移动 1 位得到 101，
     // 这两个数每个位都不同，因此异或得到的结果为 1111
     public boolean hasAlternatingBits(int n) {
         int a = (n ^ (n>>1));
