b283446f41d4a331f7004323f712ed019e40c14e
Kevin <zuoyenan@gmail.com>
2019-07-09 21:00:12 +0800

    leetcode Num476

--- src/com/leetcode/math/Num476_NumberComplement.java
+++ src/com/leetcode/math/Num476_NumberComplement.java
@@ -9,15 +9,19 @@ public class Num476_NumberComplement {
         if (num == 0)
             return 1;
         int mask = 1 << 30;
+        // 不断将mask中的1右移，知道1刚好到num二进制表示1的最高位：比如对于num为101时，mask为100才停止右移
         while ((num & mask) == 0)
             mask >>= 1;
+        // 将mask左移一位再减1就能得到想要的mask，如100-->1000-->111
         mask = (mask << 1) - 1;
         return num ^ mask;
 
     }
 
     public static void main(String[] args) {
-
+        int test = 5;
+        Num476_NumberComplement n = new Num476_NumberComplement();
+        n.findComplement(test);
     }
 
 }
