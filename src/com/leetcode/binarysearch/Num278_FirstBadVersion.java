32cc60724b82de4924a04ff17106ae3038b46bbf
Kevin <zuoyenan@gmail.com>
2019-07-29 22:17:54 +0800

    leetcode Num278

---
+++ src/com/leetcode/binarysearch/Num278_FirstBadVersion.java
@@ -0,0 +1,26 @@
+package com.leetcode.binarysearch;
+
+// 给定一个元素 n 代表有 [1, 2, ..., n] 版本，在第 x 位置开始出现错误版本，导致后面的版本都错误。
+// 可以调用 isBadVersion(int x) 知道某个版本是否错误，要求找到第一个错误的版本
+
+public class Num278_FirstBadVersion {
+    // 使用二分查找
+//    public int firstBadVersion(int n) {
+//        int l=1, h=n;
+//        while(l<h){
+//            int mid = l+(h-l)/2;
+//            if(isBadVersion(mid))
+//                h = mid;
+//            else
+//                l = mid+1;
+//
+//        }
+//
+//        return l;
+//    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
