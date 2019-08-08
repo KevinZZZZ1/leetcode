0efeb5a7a3ac1d5c3801e7ff7d5f3ea5eccda4dc
Kevin <zuoyenan@gmail.com>
2019-08-08 22:23:46 +0800

    leetcode Num257

---
+++ src/com/leetcode/backtracking/Num257_BinaryTreePaths.java
@@ -0,0 +1,60 @@
+package com.leetcode.backtracking;
+
+
+import java.util.ArrayList;
+import java.util.List;
+// 给定一个二叉树，找出所有根到叶子的路径
+public class Num257_BinaryTreePaths {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+        List<Integer> values = new ArrayList<>();
+        backtracking(root, values, paths);
+        return paths;
+    }
+
+    private void backtracking(TreeNode node, List<Integer> values, List<String> paths) {
+        if (node == null) {
+            return;
+        }
+        values.add(node.val);
+        if (isLeaf(node)) {
+            paths.add(buildPath(values));
+        } else {
+            backtracking(node.left, values, paths);
+            backtracking(node.right, values, paths);
+        }
+        values.remove(values.size() - 1);
+    }
+
+    private boolean isLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+
+    private String buildPath(List<Integer> values) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < values.size(); i++) {
+            str.append(values.get(i));
+            if (i != values.size() - 1) {
+                str.append("->");
+            }
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+  }
