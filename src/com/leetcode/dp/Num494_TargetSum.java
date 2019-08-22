ed4a40cf9c48f73108d90b8c3eb15b4f97506648
Kevin <zuoyenan@gmail.com>
2019-08-22 11:13:00 +0800

    leetcode Num494

--- src/com/leetcode/dp/Num494_TargetSum.java
+++ src/com/leetcode/dp/Num494_TargetSum.java
@@ -3,6 +3,9 @@ package com.leetcode.dp;
 public class Num494_TargetSum {
 
     public int findTargetSumWays(int[] nums, int S) {
+        if (nums==null || nums.length==0)
+            return 0;
+
         return 0;
     }
 
